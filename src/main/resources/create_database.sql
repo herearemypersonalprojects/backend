15bf7b1680d35c269bad41104dee4f070b03b45c
telecomparistech <ing.dev.java@gmail.com>
2015-08-06 23:04:29 +0200

    encoding utf8

--- src/main/resources/create_database.sql
+++ src/main/resources/create_database.sql
@@ -1,5 +1,5 @@
 ~$ mysql -u root -proot
-mysql> create database map;
+mysql> create database map character set utf8;
 mysql> create user map;
 mysql> set password for map = password("map");
 mysql> grant all on map.* to map;
