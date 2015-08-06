57d1056866782bc3020f9f9d4d04971f5c27ae8a
lequocanh <quocanh263@gmail.com>
2015-08-06 13:31:25 +0200

    upload file & call from different domains in ajax

---
+++ src/main/java/com/idlookid/controller/FileController.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.idlookid.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author quocanh
+ *
+ */
+@RestController
+public class FileController {
+	 
+    @RequestMapping(value="/upload", method=RequestMethod.GET)
+    public @ResponseBody String provideUploadInfo() {
+        return "You can upload a file by posting to this same URL.";
+    }
+
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(@RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                String name = "/home/quocanh/files/" + file.getName();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name)));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + "!";
+            } catch (Exception e) {
+                return "You failed to upload " + file.getName() + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + file.getName() + " because the file was empty.";
+        }
+    }
+}
