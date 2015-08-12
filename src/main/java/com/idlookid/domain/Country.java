fad307af682f6e2180ab4e2cab57ea1b79f5692c
telecomparistech <ing.dev.java@gmail.com>
2015-08-12 15:54:30 +0200

    add list of countries

---
+++ src/main/java/com/idlookid/domain/Country.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.idlookid.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author quocanh
+ *
+ */
+@Entity
+@Table(name = "country")
+public class Country {
+	
+}
