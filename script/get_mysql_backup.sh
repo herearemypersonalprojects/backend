7a19b151b25060f6bb39e787771d4a2e618eedc7
lequocanh <quocanh263@gmail.com>
2015-08-25 12:38:36 +0200

    adapted name of db

--- script/get_mysql_backup.sh
+++ script/get_mysql_backup.sh
@@ -3,7 +3,7 @@
 # login
 
 
-sftp quocanh@bandoviet.net:backup/mysql/01/mysql-2015-08-08.bz2
+sftp quocanh@bandoviet.net:backup/mysql/01/mysql-`date +%Y-%m-%d`.bz2
 export PATH=$PATH:/usr/local/mysql/bin/
-bunzip2 < mysql-2015-08-08.bz2 | mysql -umap -pmap map 
+bunzip2 < mysql-`date +%Y-%m-%d`.bz2 | mysql -umap -pmap map 
 
